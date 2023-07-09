b2c1e0dfe4be513ff0051a01a014ad29fa97444a
Rathawut Lertsuksakda <rathawut.l@gmail.com>
2023-07-09 14:24:37 +0700

    initiate the app layout

---
+++ src/app/about/page.tsx
@@ -0,0 +1,9 @@
+const AboutPage: React.FC = () => {
+  return (
+    <div className="text-center">
+      <h1 className="text-4xl font-bold mb-4">About Me</h1>
+    </div>
+  );
+};
+
+export default AboutPage;
